d6b03a32add11fde0435743d0ce7cc5acbc88a13
Kamil <nurzynski.kamil@gmail.com>
2023-10-20 16:22:26 +0200

    all tests

--- src/test/java/TestsOrangeTvGo.java
+++ src/test/java/TestsOrangeTvGo.java
@@ -39,81 +39,81 @@ public class TestsOrangeTvGo {
 
     }
 
-//
-//    @Test
-//    public void openApp() {
-//        System.out.println("open app test");
-//        //how to chck status 200 code????????
-//    }
-//
-//    @Test
-//    public void verifyIfAllComponentsLoaded() {
-//        //what is complete app?
-//    }
-//
-//    @Test
-//    public void countBanners() throws InterruptedException {
-//        WebElement dismiss_button = driver.findElementById("com.orange.pl.orangetvgo:id/menu_login");
-//        dismiss_button.click();
-//        WebElement agreement_checkbox = driver.findElementById("com.orange.pl.orangetvgo:id/welcome_analytics_checkbox");
-//        agreement_checkbox.click();
-//        WebElement lets_start = driver.findElementById("com.orange.pl.orangetvgo:id/welcome_btn_start");
-//        lets_start.click();
-//        TouchAction touchAction = new TouchAction(driver);
-//        WebDriver.Window device_size = driver.manage().window();
-//        float screenWidth = device_size.getSize().width;
-//
-//
-//        //////Swipe right to left//////
-//        int startx = (int) screenWidth * 8 / 9;
-//        int endx = (int) screenWidth / 9;
-//        int starty = 670;
-//        int endy = 670;
-//
-//        ArrayList<String> titles = new ArrayList<String>();
-//        int offset = 0;
-//        boolean flag = true;
-//        while (flag) {
-//            Thread.sleep(2000);
-//            WebElement banner = driver.findElementByAndroidUIAutomator(
-//                    "resourceId(\"com.orange.pl.orangetvgo:id/banner_small\")");
-//
-//            banner.click();
-//            WebElement title_obj = driver.findElementById("com.orange.pl.orangetvgo:id/text_expanded");
-//            if (!titles.contains(title_obj.getText())) {
-//                titles.add(title_obj.getText());
-//                offset += 1;
-//                driver.pressKey(new KeyEvent(AndroidKey.BACK));
-//                for (int i = 0; i < offset; i++) {
-//                    Thread.sleep(2000);
-//                    touchAction.press(PointOption.point(startx, starty)).waitAction(
-//                                    WaitOptions.waitOptions(Duration.ofSeconds(2)))
-//                            .moveTo(PointOption.point(endx, endy)).release().perform();
-//                }
-//            } else {
-//                flag = false;
-//            }
-//
-//        }
-//        System.out.println("In the app is: " + titles.size() + "banners.");
-//
-//    }
-//
-//    @Test
-//    public void enterToFourthRecommendedMovie() throws InterruptedException {
-//        WebElement dismiss_button = driver.findElementById("com.orange.pl.orangetvgo:id/menu_login");
-//        dismiss_button.click();
-//        WebElement agreement_checkbox = driver.findElementById("com.orange.pl.orangetvgo:id/welcome_analytics_checkbox");
-//        agreement_checkbox.click();
-//        WebElement lets_start = driver.findElementById("com.orange.pl.orangetvgo:id/welcome_btn_start");
-//        lets_start.click();
-//        WebElement fourth_movie_image = driver.findElementByXPath("//android.view.ViewGroup[2]/androidx." +
-//                "recyclerview.widget.RecyclerView/androidx.appcompat.widget." +
-//                "LinearLayoutCompat[4]/android.widget.ImageView");
-//        System.out.println(fourth_movie_image);
-//        fourth_movie_image.click();
-//
-//    }
+
+    @Test
+    public void openApp() {
+        System.out.println("open app test");
+        //how to chck status 200 code????????
+    }
+
+    @Test
+    public void verifyIfAllComponentsLoaded() {
+        //what is complete app?
+    }
+
+    @Test
+    public void countBanners() throws InterruptedException {
+        WebElement dismiss_button = driver.findElementById("com.orange.pl.orangetvgo:id/menu_login");
+        dismiss_button.click();
+        WebElement agreement_checkbox = driver.findElementById("com.orange.pl.orangetvgo:id/welcome_analytics_checkbox");
+        agreement_checkbox.click();
+        WebElement lets_start = driver.findElementById("com.orange.pl.orangetvgo:id/welcome_btn_start");
+        lets_start.click();
+        TouchAction touchAction = new TouchAction(driver);
+        WebDriver.Window device_size = driver.manage().window();
+        float screenWidth = device_size.getSize().width;
+
+
+        //////Swipe right to left//////
+        int startx = (int) screenWidth * 8 / 9;
+        int endx = (int) screenWidth / 9;
+        int starty = 670;
+        int endy = 670;
+
+        ArrayList<String> titles = new ArrayList<String>();
+        int offset = 0;
+        boolean flag = true;
+        while (flag) {
+            Thread.sleep(2000);
+            WebElement banner = driver.findElementByAndroidUIAutomator(
+                    "resourceId(\"com.orange.pl.orangetvgo:id/banner_small\")");
+
+            banner.click();
+            WebElement title_obj = driver.findElementById("com.orange.pl.orangetvgo:id/text_expanded");
+            if (!titles.contains(title_obj.getText())) {
+                titles.add(title_obj.getText());
+                offset += 1;
+                driver.pressKey(new KeyEvent(AndroidKey.BACK));
+                for (int i = 0; i < offset; i++) {
+                    Thread.sleep(2000);
+                    touchAction.press(PointOption.point(startx, starty)).waitAction(
+                                    WaitOptions.waitOptions(Duration.ofSeconds(2)))
+                            .moveTo(PointOption.point(endx, endy)).release().perform();
+                }
+            } else {
+                flag = false;
+            }
+
+        }
+        System.out.println("In the app is: " + titles.size() + "banners.");
+
+    }
+
+    @Test
+    public void enterToFourthRecommendedMovie() throws InterruptedException {
+        WebElement dismiss_button = driver.findElementById("com.orange.pl.orangetvgo:id/menu_login");
+        dismiss_button.click();
+        WebElement agreement_checkbox = driver.findElementById("com.orange.pl.orangetvgo:id/welcome_analytics_checkbox");
+        agreement_checkbox.click();
+        WebElement lets_start = driver.findElementById("com.orange.pl.orangetvgo:id/welcome_btn_start");
+        lets_start.click();
+        WebElement fourth_movie_image = driver.findElementByXPath("//android.view.ViewGroup[2]/androidx." +
+                "recyclerview.widget.RecyclerView/androidx.appcompat.widget." +
+                "LinearLayoutCompat[4]/android.widget.ImageView");
+        System.out.println(fourth_movie_image);
+        fourth_movie_image.click();
+
+    }
 
     @Test
     public void collectCastFromFourthMovie() throws InterruptedException {
